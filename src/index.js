98940310a0432fe6d29084487d97fe5b21c7ea6a
djeytq <djeytiquimica@gmail.com>
2025-01-29 00:10:49 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const app=express();
+const app = express();
 const routes = require('./routes');
 const Migration = require('./migration');
 const cors = require('cors');
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
+const path = require('path'); // Importar o módulo path
+
 dotenv.config();
 Migration();
 app.use(cors());
@@ -11,14 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/',(req,res)=>{
-    res.json({ message: 'Hello! Welcome to my api, it has many routes to you enjoy in your frontEnd.' });
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'html/index.html')); // Corrigir para path.resolve
 });
 
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log('server is running on port '+PORT);
+app.listen(PORT, () => {
+    console.log('server is running on port ' + PORT);
 });
